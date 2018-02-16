e349a99f1e9f85ec571311f9dd317261d8a4ea61
William <WarMad2@Hotmail.com>
2018-02-16 15:01:00 -0500

    Under ReviewController, Added @RequestMapping(value = reviews)

--- src/main/java/org/wecancodeit/columbus/reviewssite/ReviewController.java
+++ src/main/java/org/wecancodeit/columbus/reviewssite/ReviewController.java
@@ -3,11 +3,19 @@ package org.wecancodeit.columbus.reviewssite;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ReviewController {
 
 	@Resource
 	ReviewRepository reviewRepo;
+
+	@RequestMapping(value = "reviews")
+	public String getAllReviews(Model model) {
+		model.addAttribute("reviews", reviewRepo.findAll());
+		return "reviews";
+	}
 
 }
