eb6031739fe471951cd9f9968e551f6c8c79aa5d
William <WarMad2@Hotmail.com>
2018-02-16 11:12:09 -0500

    Created Review Class through ReviewTest Class. Started laying out underTest and other Variables to start the Constructor for Review Class.

--- src/test/java/org/wecancodeit/columbus/reviewssite/ReviewTest.java
+++ src/test/java/org/wecancodeit/columbus/reviewssite/ReviewTest.java
@@ -1,5 +1,20 @@
 package org.wecancodeit.columbus.reviewssite;
 
+import org.junit.Test;
+
 public class ReviewTest {
+	private Review underTest;
+
+	private long ID = 1111;
+	private String TITLE = "title";
+	private String URL = "url";
+	private String CATEGORY = "category";
+	private String CONTENT = "content";
+	private String DESCRIPTION = "description";
+
+	@Test
+	public void shouldConstructIdTitleImageurlCategoryContentDescription() {
+		underTest = new Review(0L, TITLE, URL, CATEGORY, CONTENT, DESCRIPTION);
+	}
 
 }
