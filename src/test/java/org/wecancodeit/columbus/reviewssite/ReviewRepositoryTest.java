a4489d5187067ba59af0ac09e66177d24ddbb088
William <WarMad2@Hotmail.com>
2018-02-16 13:19:41 -0500

    Created ReviewRepository Class through ReviewRepositoryTest Class.

--- src/test/java/org/wecancodeit/columbus/reviewssite/ReviewRepositoryTest.java
+++ src/test/java/org/wecancodeit/columbus/reviewssite/ReviewRepositoryTest.java
@@ -1,5 +1,14 @@
 package org.wecancodeit.columbus.reviewssite;
 
+import org.junit.Before;
+
 public class ReviewRepositoryTest {
+
+	private ReviewRepository underTest;
+
+	@Before
+	public void setup() {
+
+	}
 
 }
