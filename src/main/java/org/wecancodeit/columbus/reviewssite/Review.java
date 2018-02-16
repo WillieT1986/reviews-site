3ed68fc0c5c1e6415b239bb0b5b41c671665637e
William <WarMad2@Hotmail.com>
2018-02-16 11:13:56 -0500

    Created constructor for Review Class. Tested Constructor through ReviewTest Class.

--- src/main/java/org/wecancodeit/columbus/reviewssite/Review.java
+++ src/main/java/org/wecancodeit/columbus/reviewssite/Review.java
@@ -2,4 +2,7 @@ package org.wecancodeit.columbus.reviewssite;
 
 public class Review {
 
+	public Review(long l, String tITLE, String uRL, String cATEGORY, String cONTENT, String dESCRIPTION) {
+	}
+
 }
