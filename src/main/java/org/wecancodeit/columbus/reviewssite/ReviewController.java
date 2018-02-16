2f594d25b1d5f59d6015e49988596dc580677a1a
William <WarMad2@Hotmail.com>
2018-02-16 14:54:27 -0500

    Added @Resource - ReviewRepository reviewRepo; under ReviewController

--- src/main/java/org/wecancodeit/columbus/reviewssite/ReviewController.java
+++ src/main/java/org/wecancodeit/columbus/reviewssite/ReviewController.java
@@ -1,8 +1,13 @@
 package org.wecancodeit.columbus.reviewssite;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ReviewController {
+
+	@Resource
+	ReviewRepository reviewRepo;
 
 }
