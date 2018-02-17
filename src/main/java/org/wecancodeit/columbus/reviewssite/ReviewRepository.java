d3bd6f504e83f9bc173814f63f36ad8aa406e6fa
William <WarMad2@Hotmail.com>
2018-02-17 00:40:33 -0500

    Minor changes

--- src/main/java/org/wecancodeit/columbus/reviewssite/ReviewRepository.java
+++ src/main/java/org/wecancodeit/columbus/reviewssite/ReviewRepository.java
@@ -20,22 +20,21 @@ public class ReviewRepository {
 						+ " is known to other students as Black Reaper. They must rely on one another as these two solve the Ghost Ship & The Alchemist.");
 
 		Review anime2 = new Review(2L, "D.Gray-man", "./images/dgrayman.jpeg",
-				"Licensed by Funimation, Episodes: 103, PG-13 - Teens 13 or older",
 				"Action, Adventure, Comedy, Super Power, Demons, Shounen",
+				"Licensed by Funimation, Episodes: 103, PG-13 - Teens 13 or older",
 				"The Millennium Earl is back and wrecking havoc on Earth once again with his mechanical demons known as Akuma.,"
 						+ "Allen Walker, a young boy raised by General Cross of the Black Order along with his new friends from the Order fight against the Millennium Earl.,"
 						+ " Can Allen and his friends save the world?");
 
 		Review anime3 = new Review(3L, "Is It Wrong to Try to Pick Up Girls in a Dungeon?", "./images/dungeon.jpg",
-				"Licensed by Sentai Filmworks, Episodes: 13, PG-13 - Teens 13 or older",
 				"Action, Adventure, Comedy, Romance, Fantasy",
+				"Licensed by Sentai Filmworks, Episodes: 13, PG-13 - Teens 13 or older",
 				"This story is about a young boy named Bell who's under the protection of the Goddess Hestia.,"
 						+ "Bell explores the Dungeon which is underground, with a tower built on top of it which is managed by The Guild and other varios Gods & Goddesses.,"
 						+ " Bell hopes to one day catch up to a girl that he likes who's at the top of her class when it comes to being an Adventurer.");
 
-		Review manga = new Review(4L, "Übel Blatt", "./images/ubel.jpg",
+		Review manga = new Review(4L, "Übel Blatt", "./images/ubel.jpg", "Action, Adventure, Ecchi, Fantasy, Seinen",
 				"Serialized by Big Gangan, Authored by Shiono, Etorouji, Unknown Volumes & Chapters",
-				"Action, Adventure, Ecchi, Fantasy, Seinen",
 				"This story is about a boy named Kóinzell who's much older than he seems. Once know to be apart of a group of,"
 						+ " Hero's who ended up being betrayed. Kóinzell, seeks vengeance against the Hero's who betrayed him and his comrades and made them out to be traitors.,"
 						+ "Kóinzell is not just an object of fear but also of hope to those around him.");
