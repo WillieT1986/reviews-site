be0d69926711be74fc0b34a0aaba991b6e3e004a
William <WarMad2@Hotmail.com>
2018-02-16 22:43:56 -0500

    Minor chanes to code. Cleaned it up some.

--- src/main/java/org/wecancodeit/columbus/reviewssite/ReviewRepository.java
+++ src/main/java/org/wecancodeit/columbus/reviewssite/ReviewRepository.java
@@ -12,13 +12,18 @@ public class ReviewRepository {
 	Map<Long, Review> reviews = new HashMap<>();
 
 	public ReviewRepository() {
-		Review anime = new Review(1L, "Gosick", "./images/gosick.jpg",
+		Review anime = new Review(1L, "Gosick", "./images/gosick.jpg", "Mystery, Historical, Drama, Romance",
 				"Licensed by Funimation, Episodes: 24, Rated R - 17+ (violence & profanity)",
-				"Mystery, Historical, Drama, Romance",
-				"A student by the name of Kujou, Kazuya attends a school where he comes across a young girl at the top of a towering Library where they end up talking about Mysteris and Legends. Solving crime scenes and assisting a detective while she remains at the top of the Library using her foresight to help solve crime while Kazuya is known to other students as Black Reaper. They must rely on one another as these two solve the Ghost Ship & The Alchemist.");
+				"A student by the name of Kujou, Kazuya attends a school where he comes across a young girl at the top of a,"
+						+ " towering Library where they end up talking about Mysteris and Legends. Solving crime scenes and assisting,"
+						+ " a detective while she remains at the top of the Library using her foresight to help solve crime while Kazuya,"
+						+ " is known to other students as Black Reaper. They must rely on one another as these two solve the Ghost Ship & The Alchemist.");
+
 		Review game = new Review(2L, "Dragon Age", "image", "Anime Content", "Anime Category", "Anime Description");
+
 		Review manga = new Review(3L, "Is It Wrong To Pick Up Girls in the Dungeon?", "image", "Anime Content",
 				"Anime Category", "Anime Description");
+
 		Review anime2 = new Review(4L, "Ubel Blight", "image", "Manga Content", "Manga Category", "Manga Description");
 
 		reviews.put(anime.getId(), anime);
